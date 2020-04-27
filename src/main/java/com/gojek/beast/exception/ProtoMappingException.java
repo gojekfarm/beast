f8ecd23bdd2344d5c3dd1fc403696d89676753b2
mauliksoneji <maulik.soneji@go-jek.com>
2020-04-27 17:34:31 +0530

    [Maulik] catch BQPartitionKeyNotSpecified and BQSchemaMappingException in ProtoUpdateListener

--- src/main/java/com/gojek/beast/exception/ProtoMappingException.java
+++
@@ -1,7 +0,0 @@
-package com.gojek.beast.exception;
-
-public class ProtoMappingException extends RuntimeException {
-    public ProtoMappingException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/gojek/beast/protomapping/ProtoUpdateListener.java
+++ src/main/java/com/gojek/beast/protomapping/ProtoUpdateListener.java
@@ -7,6 +7,8 @@ import com.gojek.beast.config.ProtoMappingConfig;
 import com.gojek.beast.config.StencilConfig;
 import com.gojek.beast.converter.ConsumerRecordConverter;
 import com.gojek.beast.converter.RowMapper;
+import com.gojek.beast.exception.BQPartitionKeyNotSpecified;
+import com.gojek.beast.exception.BQSchemaMappingException;
 import com.gojek.beast.exception.BQTableUpdateFailure;
 import com.gojek.beast.exception.ProtoNotFoundException;
 import com.gojek.beast.models.BQField;
@@ -86,7 +88,7 @@ public class ProtoUpdateListener extends com.gojek.de.stencil.cache.ProtoUpdateL
             ProtoField protoField = protoFieldFactory.getProtoField();
             protoField = protoMappingParser.parseFields(protoField, proto, StencilUtils.getAllProtobufDescriptors(newDescriptors), StencilUtils.getTypeNameToPackageNameMap(newDescriptors));
             updateProtoParser(protoField);
-        } catch (BigQueryException | ProtoNotFoundException | IOException e) {
+        } catch (BigQueryException | ProtoNotFoundException | BQSchemaMappingException | BQPartitionKeyNotSpecified | IOException e) {
             String errMsg = "Error while updating bigquery table on callback:" + e.getMessage();
             log.error(errMsg);
             statsClient.increment("bq.table.upsert.failures");
